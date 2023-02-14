d6324a7c2f482ec098576a9dbf0adf4350556257
paragpatil1999 <paragptil1999@gmail.com>
2023-02-14 18:38:24 +0530

    checking CI pipeline 1

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ else if (!config.get('MongoDBPassword')) {
   console.error('FATAL ERROR: MongoDBPassword is not defined.');
   process.exit(1);
 }
-// checking CI pipeline
+// checking CI pipeline 1
 
 const dbUrl = `mongodb+srv://${config.get('MongoDBUserName')}:${config.get('MongoDBPassword')}@splitwise.eneyypj.mongodb.net/SplitWiseDev?retryWrites=true&w=majority`;
 
